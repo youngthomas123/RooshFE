7d13aeee3a1d1d8cd843e1569ec5926f6f1176c1
Timothy Axel W <i528900@fhict.nl>
2023-11-26 16:49:56 +0100

    update general for provider details

--- src/components/ProvidersComponent/Details/DetailGeneral.jsx
+++ src/components/ProvidersComponent/Details/DetailGeneral.jsx
@@ -1,7 +1,7 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Container, Grid, IconButton, Paper, Stack, Tooltip, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-
+import EditIcon from '@mui/icons-material/Edit';
 const DetailGeneral = () => {
     const { rowData } = useParams();
     const decodedData = JSON.parse(decodeURIComponent(rowData));
@@ -10,7 +10,63 @@ const DetailGeneral = () => {
 
     return (
         <Box>
-            <Typography variant="h5" gutterBottom sx={{ color: 'grey' }}>General</Typography>
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                <Grid item xs={6}>
+                    <Paper elevation={3}>
+                        <Container sx={{py : 2}}>
+                        <Stack
+                            direction="row"
+                            justifyContent="space-between"
+                            alignItems="center"
+                            spacing={2}
+                        >
+                            <Typography variant="h5" gutterBottom sx={{ color: 'grey', fontWeight: 'bold' }}>Basic Info</Typography>
+                            <Tooltip title="Edit">
+                                <IconButton>
+                                    <EditIcon sx={{ color: '#ff7017' }} />
+                                </IconButton>
+                            </Tooltip>
+                        </Stack>
+                        
+                        <Typography variant="h6" gutterBottom sx={{ color: 'grey', mt: 2 }}>Name</Typography>
+                        {/* <Typography variant="body1" gutterBottom sx={{ color: 'grey' }}>{decodedData.name}</Typography> */}
+                        <Typography variant="body1" gutterBottom sx={{ color: 'grey', mt:-1 }}>name placeholder</Typography> 
+
+                        <Typography variant="h6" gutterBottom sx={{ color: 'grey', mt: 2 }}>Airport</Typography>
+                        {/* <Typography variant="body1" gutterBottom sx={{ color: 'grey' }}>{decodedData.name}</Typography> */}
+                        <Typography variant="body1" gutterBottom sx={{ color: 'grey', mt:-1 }}>airport placeholder</Typography> 
+                        </Container>
+                    </Paper>
+                </Grid>
+                <Grid item xs={6}>
+                    <Paper elevation={3}>
+                        <Container sx={{py : 2}}>
+                        <Stack
+                            direction="row"
+                            justifyContent="space-between"
+                            alignItems="center"
+                            spacing={2}
+                        >
+                            <Typography variant="h5" gutterBottom sx={{ color: 'grey', fontWeight: 'bold' }}>Business Address</Typography>
+                            <Tooltip title="Edit">
+                                <IconButton>
+                                    <EditIcon sx={{ color: '#ff7017' }} />
+                                </IconButton>
+                            </Tooltip>
+                        </Stack>
+                        
+                        <Typography variant="h6" gutterBottom sx={{ color: 'grey', mt: 2 }}>Street</Typography>
+                        {/* <Typography variant="body1" gutterBottom sx={{ color: 'grey' }}>{decodedData.name}</Typography> */}
+                        <Typography variant="body1" gutterBottom sx={{ color: 'grey', mt:-1 }}>street placeholder</Typography> 
+
+                        <Typography variant="h6" gutterBottom sx={{ color: 'grey', mt: 2 }}>City</Typography>
+                        {/* <Typography variant="body1" gutterBottom sx={{ color: 'grey' }}>{decodedData.name}</Typography> */}
+                        <Typography variant="body1" gutterBottom sx={{ color: 'grey', mt:-1 }}>City placeholder</Typography> 
+                        </Container>
+                    </Paper>
+                </Grid>
+            </Grid>
+
         </Box>
     );
 }
